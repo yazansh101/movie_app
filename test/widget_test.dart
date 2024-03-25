7b64a40dd9d6309d1a9acbd3e5aaf5064c7a75b4
yazanshlash <yazanshlash101@gmail.com>
2024-03-25 18:12:02 +0300

    setup architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yazan_movie_db_task/main.dart';
+import 'package:yazan_movie_db_task/core/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
