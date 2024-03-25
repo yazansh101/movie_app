66295f4c6bd769d0abd9161783b072fdd5bcd287
yazanshlash <yazanshlash101@gmail.com>
2024-03-25 18:56:38 +0300

    add core files

---
+++ lib/core/mapper/converter.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:yazan_movie_db_task/features/top_rated_movies/domain/entities/top_rated_movie.dart';
+
+
+
+// class ConvertFromJsonToReward
+//     extends JsonConverter<MovieEntity, Map<String, dynamic>> {
+//   @override
+//   MovieEntity fromJson(Map<String, dynamic> json) =>
+//       RewardModel.fromJson(json);
+
+//   @override
+//   Map<String, dynamic> toJson(MovieEntity object) =>
+//       (object as RewardModel).toJson();
+
+//   const ConvertFromJsonToReward();
+// }
+
